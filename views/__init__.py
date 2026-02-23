f1c6b515bb31c5db5f552ad40b54c4ddf39c795d
João Gerhard <joaocabecafeijao2.0@gmail.com>
2026-02-22 22:25:44 -0300

    melhoria da embed do bump notifier

--- views/__init__.py
+++ views/__init__.py
@@ -2,5 +2,6 @@ from .welcome_view import Components as WelcomeComponents
 from .colors import ColorsView
 from .normal_colors import NormalColorsView
 from .rare_ping import Components, RarePingView
+from .bumpview import BumpComponents
 
-__all__ = ["WelcomeComponents", "ColorsView", "NormalColorsView", "Components", "RarePingView"]
+__all__ = ["WelcomeComponents", "ColorsView", "NormalColorsView", "Components", "RarePingView", "BumpComponents"]
