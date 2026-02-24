e948d84a2ad66af8a56716876809d5a2f7f7c60e
João Gerhard <joaocabecafeijao2.0@gmail.com>
2026-02-24 17:21:37 -0300

    bump notifier funcionando

--- events/message_handler.py
+++ events/message_handler.py
@@ -17,7 +17,14 @@ async def handle_bump_detection(bot, message):
         
     print("Bump done detected")
     
-    await message.channel.send(f"✅ Bump! Vou te lembrar em 2 horas para dar o próximo bump! ⏰ {message.author.mention}")
+    async for msg in message.channel.history(limit=10, before=message):
+        if msg.content.strip() == "/bump" and not msg.author.bot:
+            bumper = msg.author.mention
+            break
+    else:
+        bumper = "alguém"
+    
+    await message.channel.send(f"✅ Bump detectado! Obrigado {bumper}! Vou lembrar em 2 horas para dar o próximo bump! ⏰")
     
     from events.bump import bump_reminder
     import asyncio
